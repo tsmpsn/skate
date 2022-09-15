28e5657642fa3d6e47d51c883aeaa805811cec53
tsmpsn <tomsimpson11@hotmail.co.uk>
2022-09-16 00:26:05 +0200

    Further clean up

--- index.js
+++ index.js
@@ -4,24 +4,19 @@ const moment = require("moment");
 
 const app = express();
 
-// Can remove if I use ES6
 app.set("view engine","jade");
   
 app.get("/", async (req, res) => {
-
     const auth = new google.auth.GoogleAuth({
         keyFile: "credentials.json",
         scopes: "https://www.googleapis.com/auth/spreadsheets"
     });
-
     const client = await auth.getClient();
 
     // Create client instance for auth
-
     const googleSheets = google.sheets({version: "v4", auth: client});
 
     // Get spreadsheet metadata
-
     const spreadsheetId = "1E5gE-qTzks3oG--GCaA0bDM7ulBN4Jap8B_Hlv4H_bo";
 
     const metaData = await googleSheets.spreadsheets.get({
@@ -30,17 +25,14 @@ app.get("/", async (req, res) => {
     });
 
     // Read rows from spreadsheet
-
     const getRows = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
         range: "bonker-lastresortab!E:I"
     });
     
+    // Remove header row
     getRows.data.values.shift();
-
-    // ES6
-    // res.send(createTable2(getRows.data.values));
     
     // Run in Jade
     res.render('shoes', { shoeList: getRows.data.values});
